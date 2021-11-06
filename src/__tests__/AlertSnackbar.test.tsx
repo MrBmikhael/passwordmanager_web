158d1f3ca6767f896e0f421cbcdbc017bdf27c20
Bishoy Mikhael <7573568+MrBmikhael@users.noreply.github.com>
2021-11-06 01:13:54 -0700

    Fixed unit test

--- src/__tests__/AlertSnackbar.test.tsx
+++ src/__tests__/AlertSnackbar.test.tsx
@@ -1,41 +1,41 @@
 import React from 'react'
 import { cleanup, render, fireEvent, screen, waitFor } from '@testing-library/react'
 import { AlertSnackbar } from '../Components/AlertSnackbar/AlertSnackbar'
-import { AlertStatus } from '../Redux/Constants/UIConstants'
+import { SnackbarAlertStatus } from '../Redux/UI/Snackbar/SnackbarConstants'
 
 afterEach(cleanup)
 
 test('renders alert', () => {
-  const { getByText } = render(<AlertSnackbar message={'Test Alert'} status={AlertStatus.success} />)
+  const { getByText } = render(<AlertSnackbar message={'Test Alert'} status={SnackbarAlertStatus.success} />)
   expect(getByText('Test Alert')).toBeInTheDocument()
 })
 
 test('success and close icon visible', () => {
-  const { queryByTestId } = render(<AlertSnackbar message={'Test Alert'} status={AlertStatus.success} />)
+  const { queryByTestId } = render(<AlertSnackbar message={'Test Alert'} status={SnackbarAlertStatus.success} />)
   expect(queryByTestId('CloseIcon')).toBeInTheDocument()
   expect(queryByTestId('SuccessOutlinedIcon')).toBeInTheDocument()
 })
 
 test('warning and close icon visible', () => {
-  const { queryByTestId } = render(<AlertSnackbar message={'Test Alert'} status={AlertStatus.warning} />)
+  const { queryByTestId } = render(<AlertSnackbar message={'Test Alert'} status={SnackbarAlertStatus.warning} />)
   expect(queryByTestId('CloseIcon')).toBeInTheDocument()
   expect(queryByTestId('ReportProblemOutlinedIcon')).toBeInTheDocument()
 })
 
 test('info and close icon visible', () => {
-  const { queryByTestId } = render(<AlertSnackbar message={'Test Alert'} status={AlertStatus.info} />)
+  const { queryByTestId } = render(<AlertSnackbar message={'Test Alert'} status={SnackbarAlertStatus.info} />)
   expect(queryByTestId('CloseIcon')).toBeInTheDocument()
   expect(queryByTestId('InfoOutlinedIcon')).toBeInTheDocument()
 })
 
 test('error and close icon visible', () => {
-  const { queryByTestId } = render(<AlertSnackbar message={'Test Alert'} status={AlertStatus.error} />)
+  const { queryByTestId } = render(<AlertSnackbar message={'Test Alert'} status={SnackbarAlertStatus.error} />)
   expect(queryByTestId('CloseIcon')).toBeInTheDocument()
   expect(queryByTestId('ErrorOutlineIcon')).toBeInTheDocument()
 })
 
 test('close button closes alert', () => {
-  render(<AlertSnackbar message={'Test Alert'} status={AlertStatus.error} />)
+  render(<AlertSnackbar message={'Test Alert'} status={SnackbarAlertStatus.error} />)
   expect(screen.queryByTestId('CloseIcon')).toBeInTheDocument()
   expect(screen.getByText('Test Alert')).toBeInTheDocument()
 
