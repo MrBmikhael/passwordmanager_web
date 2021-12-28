524f22381e32a74a4d66c1e504aaecd96be65341
Bishoy Mikhael <7573568+MrBmikhael@users.noreply.github.com>
2021-12-27 20:46:47 -0800

    test(unit tests): minor fixes

--- src/__tests__/AES.test.ts
+++ src/__tests__/AES.test.ts
@@ -1,8 +1,8 @@
 import AES from '../Security/AES'
 
-const key = "password123"
-const secret = "qwerty"
-let encryptedText = ""
+const key = 'password123'
+const secret = 'qwerty'
+let encryptedText = ''
 
 test('Encrypt string', () => {
   encryptedText = AES.encrypt(secret, key)
@@ -13,4 +13,3 @@ test('Decrypt string', () => {
   const txt = AES.decrypt(encryptedText, key)
   expect(txt).toEqual(secret)
 })
-
--- src/__tests__/AlertSnackbar.test.tsx
+++ src/__tests__/AlertSnackbar.test.tsx
@@ -1,41 +1,43 @@
 import React from 'react'
-import { cleanup, render, fireEvent, screen, waitFor } from '@testing-library/react'
+import {
+  cleanup, render, fireEvent, screen, waitFor
+} from '@testing-library/react'
 import { AlertSnackbar } from '../Components/AlertSnackbar/AlertSnackbar'
 import { SnackbarAlertStatus } from '../Redux/Store/UI/Snackbar/SnackbarConstants'
 
 afterEach(cleanup)
 
 test('renders alert', () => {
-  const { getByText } = render(<AlertSnackbar message={'Test Alert'} status={SnackbarAlertStatus.success} />)
+  const { getByText } = render(<AlertSnackbar message="Test Alert" status={SnackbarAlertStatus.success} />)
   expect(getByText('Test Alert')).toBeInTheDocument()
 })
 
 test('success and close icon visible', () => {
-  const { queryByTestId } = render(<AlertSnackbar message={'Test Alert'} status={SnackbarAlertStatus.success} />)
+  const { queryByTestId } = render(<AlertSnackbar message="Test Alert" status={SnackbarAlertStatus.success} />)
   expect(queryByTestId('CloseIcon')).toBeInTheDocument()
   expect(queryByTestId('SuccessOutlinedIcon')).toBeInTheDocument()
 })
 
 test('warning and close icon visible', () => {
-  const { queryByTestId } = render(<AlertSnackbar message={'Test Alert'} status={SnackbarAlertStatus.warning} />)
+  const { queryByTestId } = render(<AlertSnackbar message="Test Alert" status={SnackbarAlertStatus.warning} />)
   expect(queryByTestId('CloseIcon')).toBeInTheDocument()
   expect(queryByTestId('ReportProblemOutlinedIcon')).toBeInTheDocument()
 })
 
 test('info and close icon visible', () => {
-  const { queryByTestId } = render(<AlertSnackbar message={'Test Alert'} status={SnackbarAlertStatus.info} />)
+  const { queryByTestId } = render(<AlertSnackbar message="Test Alert" status={SnackbarAlertStatus.info} />)
   expect(queryByTestId('CloseIcon')).toBeInTheDocument()
   expect(queryByTestId('InfoOutlinedIcon')).toBeInTheDocument()
 })
 
 test('error and close icon visible', () => {
-  const { queryByTestId } = render(<AlertSnackbar message={'Test Alert'} status={SnackbarAlertStatus.error} />)
+  const { queryByTestId } = render(<AlertSnackbar message="Test Alert" status={SnackbarAlertStatus.error} />)
   expect(queryByTestId('CloseIcon')).toBeInTheDocument()
   expect(queryByTestId('ErrorOutlineIcon')).toBeInTheDocument()
 })
 
 test('close button closes alert', () => {
-  render(<AlertSnackbar message={'Test Alert'} status={SnackbarAlertStatus.error} />)
+  render(<AlertSnackbar message="Test Alert" status={SnackbarAlertStatus.error} />)
   expect(screen.queryByTestId('CloseIcon')).toBeInTheDocument()
   expect(screen.getByText('Test Alert')).toBeInTheDocument()
 
@@ -50,5 +52,4 @@ test('close button closes alert', () => {
     timeout: 1000,
     interval: 250
   })
-
 })
